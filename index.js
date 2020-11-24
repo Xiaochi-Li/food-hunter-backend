88b50c96ee36c2df14c20af50a0274a33b74619a
xiaochi li <sean.li@comparethemarket.com.au>
2020-11-24 16:28:49 +1000

    feat: add get list by index component

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ app.get("/api/lists", (req, res) => {
     res.send([{ a: "noodle" }, { a: "chicken" }]);
 });
 
+app.get("/api/list/:index", (req, res) => {
+    const list = [{ a: "noodle" }, { a: "chicken" }];
+    res.send(list[req.params.index])
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on oprt ${port}...`));
